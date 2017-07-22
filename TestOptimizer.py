02ccf0c4d27cc85b8d6e787c07d1143981fd09f8
iehuuy_VM_ubuntu14LTS <yuuhei.hayashi@gmail.com>
2017-07-22 19:05:36 +0900

    modified

--- TestOptimizer.py
+++ TestOptimizer.py
@@ -4,30 +4,43 @@ import matplotlib.pyplot as plt
 
 def func(variable_vector):
     return [
-        2*variable_vector[0]**2 + variable_vector[1]**2 + 3*variable_vector[0] + 2*variable_vector[1]
+        variable_vector[0]**2 + variable_vector[1]**2
         ]
 
+class Search_set:
+    __search_axis = []
+    __search_start_point = ""
+    
+    def __init__(self,search_axis,search_start_point):
+        self.__search_axis = search_axis
+        self.__search_start_point = search_start_point
+    
+    def get_search_axis(self):
+        return self.__search_axis
+    
+    def get_search_start_point(self):
+        return self.__search_start_point
+    
 
 if __name__ == '__main__':
-    points = np.random.rand(30, 2)
-    print points
     
     initial_variable_vector = np.array([0.0,0.0])
     variable_vector = np.array([0.0,0.0])
     resource_vector = np.array([0.0])
-    resource_constraint_vector = np.array([500.0])
+    resource_constraint_vector = np.array([10000.0])
     feasible_variable_vector_list = []
     allowable_error = 0.1
     initial_flag = 0
     delta = 1
+    
+    search_sets = [Search_set(np.array([1.0,0]),np.array([0,0])), Search_set(np.array([0,1.0]),np.array([0,0]))]
         
-    search_axes = [np.array([1.0,0]),np.array([0,1.0])]
-
      #serach
-    for search_axis in search_axes:
+    for search_set in search_sets:
         
         #set start point
-        variable_vector = initial_variable_vector
+        search_axis = search_set.get_search_axis()
+        variable_vector = search_set.get_search_start_point()
         resource_vector = func(variable_vector)
         if func(variable_vector)[0] < resource_constraint_vector[0]:
             feasible_variable_vector_list.append(variable_vector.tolist())
@@ -45,21 +58,49 @@ if __name__ == '__main__':
         while True:
             if abs(resource_vector[0] - resource_constraint_vector[0]) < allowable_error:
                 break
+            
             else:
-                variable_vector = variable_vector + search_axis*(resource_constraint_vector[0] - func(variable_vector)[0]) * np.linalg.norm(variable_vector - previous_variable_vector)/(func(variable_vector)[0] - func(previous_variable_vector)[0])
+                if np.dot(previous_variable_vector,search_axis) < np.dot(variable_vector,search_axis):
+                    new_vector = variable_vector + search_axis*(resource_constraint_vector[0] - resource_vector[0]) * np.linalg.norm(variable_vector - previous_variable_vector)/(resource_vector[0] - previous_resource_vector[0])
+                else:
+                    new_vector = variable_vector - search_axis*(resource_constraint_vector[0] - resource_vector[0]) * np.linalg.norm(variable_vector - previous_variable_vector)/(resource_vector[0] - previous_resource_vector[0])
+                previous_variable_vector = variable_vector
+                previous_resource_vector = resource_vector
+                variable_vector = new_vector
                 resource_vector = func(variable_vector)
-            if func(variable_vector)[0] < resource_constraint_vector[0]:
-                feasible_variable_vector_list.append(variable_vector.tolist())
-                print "variable : " + str(variable_vector) + " , resource : " + str(resource_vector)              
-    
-    print feasible_variable_vector_list
+                print "variable : " + str(variable_vector) + " , resource : " + str(resource_vector) 
 
+            if resource_vector[0] < resource_constraint_vector[0]:
+                feasible_variable_vector_list.append(variable_vector.tolist())
+    
     points = np.array(feasible_variable_vector_list)
-    print points  
     hull = ConvexHull(points)
+    vertices = hull.vertices
+
+'''    
+    for i in range(1,len(vertices) - 1):
+        endpoint_a = points[vertices[i]]
+        endpoint_b = points[vertices[i + 1]]
+        axis = endpoint_b - endpoint_a
+        axis = axis / np.linalg.norm(axis)
+        previous_variable_vector = endpoint_a
+        previous_resource_vector =  func(previous_variable_vector)
+        variable_vector = variable_vector + search_axis*delta
+        resource_vector = func(variable_vector)
+        while True:
+            if abs(resource_vector[0] - previous_resource_vector[0]) < allowable_error:
+                break
+            else:
+                
+                variable_vector = variable_vector - search_axis*(func(variable_vector)[0]) * np.linalg.norm(variable_vector - previous_variable_vector)/(func(variable_vector)[0] - func(previous_variable_vector)[0])
+                
+                
+
+
     plt.plot(points[:,0], points[:,1], 'o')
     for simplex in hull.simplices:
-        plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+    plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
     plt.plot(points[hull.vertices,0], points[hull.vertices,1], 'r--', lw=2)
     plt.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'ro')
     plt.show()
+'''
